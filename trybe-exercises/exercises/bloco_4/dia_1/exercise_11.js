bb2522f1c5fb124d7730616797b8b4c7def2f554
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-04-22 18:23:31 -0300

    Resolução do exercício 11

---
+++ trybe-exercises/exercises/bloco_4/dia_1/exercise_11.js
@@ -0,0 +1,30 @@
+let INSS;
+let IR;
+
+let salarioBruto = 3000.00;
+
+if (salarioBruto <= 1556.64) {
+  INSS = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {
+  INSS = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82) {
+  INSS = salarioBruto * 0.11;
+} else {
+  INSS = 570.88;
+}
+
+let salarioBase = salarioBruto - INSS;
+
+if (salarioBase <= 1903.98) {
+  IR = 0;
+} else if (salarioBase <= 2826.65) {
+  IR = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase <= 3751.05) {
+  IR = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase <= 4664.68) {
+  IR = (salarioBase * 0.225) - 636.13;
+} else {
+  IR = (salarioBase * 0.275) - 869.36;
+};
+
+console.log('Salário: ' + (salarioBase - IR));
