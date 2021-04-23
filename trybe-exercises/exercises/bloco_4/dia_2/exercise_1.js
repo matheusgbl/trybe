6907534ad1b3a7ac8308a9e3a64f30890526c72d
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-04-23 14:42:58 -0300

    Criado arquivo para a realização do terceiro exercício e troca de numbers por numeros

--- trybe-exercises/exercises/bloco_4/dia_2/exercise_1.js
+++ trybe-exercises/exercises/bloco_4/dia_2/exercise_1.js
@@ -1,5 +1,5 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numeros = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let i = 0; i < numbers.length; i += 1) {
-  console.log(numbers[i]);
+for (let i = 0; i < numeros.length; i += 1) {
+  console.log(numeros[i]);
 }
--- trybe-exercises/exercises/bloco_4/dia_2/exercise_2.js
+++ trybe-exercises/exercises/bloco_4/dia_2/exercise_2.js
@@ -1,8 +1,8 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numeros = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let resultado = 0;
 
-for (let i = 0; i < numbers.length; i += 1) {
-  resultado += numbers[i];
+for (let i = 0; i < numeros.length; i += 1) {
+  resultado += numeros[i];
 };
 
 console.log(resultado);
