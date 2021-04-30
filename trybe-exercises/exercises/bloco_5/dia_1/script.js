63fc8f0f67e770f9c593c88f4febfe587b374efc
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-04-30 16:13:03 -0300

    Resolução do requisito 3

--- trybe-exercises/exercises/bloco_5/dia_1/script.js
+++ trybe-exercises/exercises/bloco_5/dia_1/script.js
@@ -1,2 +1,3 @@
 document.getElementById("paragraph1").innerHTML = "Trbalhando em algo top";
 document.getElementsByTagName("div")[0].style.backgroundColor = "rgb(76,164,109)";
+document.getElementsByTagName("div")[1].style.backgroundColor = "white";
