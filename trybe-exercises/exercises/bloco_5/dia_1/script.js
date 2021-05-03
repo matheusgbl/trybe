64a857e60ba1d76278f6eff7beaf3861ca5db505
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-05-03 15:12:22 -0300

    Resolução do requisito 5

--- trybe-exercises/exercises/bloco_5/dia_1/script.js
+++ trybe-exercises/exercises/bloco_5/dia_1/script.js
@@ -2,3 +2,9 @@ document.getElementById("paragraph1").innerHTML = "Trbalhando em algo top";
 document.getElementsByTagName("div")[0].style.backgroundColor = "rgb(76,164,109)";
 document.getElementsByTagName("div")[1].style.backgroundColor = "white";
 document.getElementsByTagName("h1")[0].innerText = "Exercício 5.1 - JavaScript";
+
+function toUppercase() {
+  let uppercase = document.getElementsByTagName("p")[0];
+  uppercase.innerHTML = uppercase.innerHTML.toUpperCase();
+}
+toUppercase();
