7978283d18f37c5f4b68f8a1a2c33c9fb70e8f7e
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-05-03 15:42:09 -0300

    Resolução do requisito 2

--- trybe-exercises/exercises/bloco_5/dia_2/script.js
+++ trybe-exercises/exercises/bloco_5/dia_2/script.js
@@ -1,3 +1,7 @@
 const elementH1 = document.createElement("h1");
 elementH1.innerHTML = 'Exercício 5.2 - JavaScript';
 document.body.appendChild(elementH1);
+
+const elementDiv = document.createElement("div");
+elementDiv.className = 'main-content';
+document.body.appendChild(elementDiv);
