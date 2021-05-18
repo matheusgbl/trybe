fae16da21184db69dca7fc9b680c17f5cb8084ad
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-05-18 08:46:30 -0300

    Função de formatação de datas

--- trybe-exercises/exercises/bloco_6/dia_2/script.js
+++ trybe-exercises/exercises/bloco_6/dia_2/script.js
@@ -8,6 +8,26 @@ function createStateOptions() {
   }
 }
 
+const picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  format: 'D/M/YYYY',
+  toString(date, format) {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+    const parts = dateString.split('/');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+
+    return new Date(year, month, day);
+  }
+});
+
 window.onload = function () {
   createStateOptions();
 }
