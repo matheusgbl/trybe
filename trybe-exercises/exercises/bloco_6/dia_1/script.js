245d1659f9dc613c636e50e726b3dc2a8171ca22
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-05-18 07:19:22 -0300

    Função que pega valor selecionado

--- trybe-exercises/exercises/bloco_6/dia_1/script.js
+++ trybe-exercises/exercises/bloco_6/dia_1/script.js
@@ -9,6 +9,108 @@ function createStateOptions() {
   }
 }
 
+let inputs = {
+  name: {
+    maxLength: 40,
+    required: true,
+  },
+  email: {
+    maxLength: 50,
+    required: true,
+  },
+  cpf: {
+    maxLength: 11,
+    required: true,
+  },
+  addres: {
+    maxLength: 200,
+    required: true,
+  },
+  city: {
+    maxLength: 28,
+    required: true,
+  },
+  state: {
+    type: 'select',
+    required: true,
+  },
+  houseType: {
+    type: 'radio',
+    required: true,
+  },
+  role: {
+    maxLength: 40,
+    required: true,
+  },
+  roleDescription: {
+    maxLength: 500,
+    required: true,
+  },
+  startDate: {
+    type: 'date',
+    required: true,
+  },
+}
+
+function validationInput(input, name) {
+  let trimmed = input.value.trim();
+  let validation = inputs[name];
+
+  if (validation.required && trimmed.length === 0) {
+    return false;
+  }
+
+  if (validation.maxLength && trimmed.length > validation.maxLength) {
+    return false;
+  }
+
+  return true;
+}
+
+function validationDate(input, name) {
+  if(input.value.length === 0) {
+    return {
+      message: 'A data não foi preenchida!'
+    }
+  }
+
+  let regex = /^\d\d\/\d\d\/\d\d\d\d$/;
+
+  if(regex.test(input.value)) {
+    return {
+      message: 'Data: formato inválido!'
+    };
+  }
+
+  let splitValue = input.value.split('/');
+  let day = splitValue[0];
+  let month = splitValue[1];
+  let year = splitValue[2];
+
+  if(day < 0 || day > 30) {
+    return {
+      message: 'Dia inválido'
+    };
+  }
+
+  if(month < 0 || month > 12) {
+    return {
+      message: 'Mês inválido'
+    };
+  }
+
+  if(year < 0) {
+    return {
+      message: 'Ano inválido'
+    };
+  }
+
+  return true;
+}
+
+function getSelectedOption(select) {
+  return select.options[select.selectedIndex];
+}
 
 window.onload = function () {
   createStateOptions();
