c68964497a568332769b2cf800a98732d6296cc0
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-05-18 07:09:38 -0300

    Funçaõ que lista estados

---
+++ trybe-exercises/exercises/bloco_6/dia_1/script.js
@@ -0,0 +1,15 @@
+function createStateOptions() {
+  let states = document.getElementById('state');
+  let stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+  
+  for (let index = 0; index < stateOptions.length; index += 1) {
+    let createOptions = document.createElement('option');
+    states.appendChild(createOptions).innerText = stateOptions[index];
+    states.appendChild(createOptions).value = stateOptions[index];
+  }
+}
+
+
+window.onload = function () {
+  createStateOptions();
+}
