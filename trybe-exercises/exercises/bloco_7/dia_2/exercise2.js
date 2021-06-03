4c9250b41522dee220493c6921166094d08fb626
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-06-03 14:17:18 -0300

    Exercícios bônus (apenas copiado)

--- trybe-exercises/exercises/bloco_7/dia_2/exercise2.js
+++ trybe-exercises/exercises/bloco_7/dia_2/exercise2.js
@@ -52,3 +52,35 @@ const verifyPair = (obj, key, value) => {
   }
   return isEqual;
 };
+
+const getNumberOfStudentsMath = (obj) => {
+  let total = 0;
+  const array = Object.keys(obj);
+  for (index in array) {
+    if(obj[array[index]].materia === 'Matemática'){
+    total += obj[array[index]].numeroEstudantes;
+    }
+  }
+  return total;
+}
+
+const getInfo = (obj, name) => {
+  const allLessons = [];
+  let allStudent = 0;
+  const array = Object.values(obj);
+  for (index in array) {
+    if (array[index].professor === name) {
+      allLessons.push(array[index].materia)
+      allStudent += array[index].numeroEstudantes;
+    }
+  }
+  return { lessons: allLessons, estudantes: allStudent };
+}
+
+const createReport = (allLessons, name) => {
+  const report = {};
+  report.professor = name;
+  Object.assign(report, getInfo(allLessons, name));
+  return report;
+}
+console.log(createReport(allLessons, 'Maria Clara'));
